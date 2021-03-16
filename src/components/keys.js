9ef5abe44ef8c70a9fde5311cd797b4744df00fa
Mako-L <macovei.gabriel.razvan@gmail.com>
2021-03-16 11:32:41 -0400

    123

--- src/components/keys.js
+++ src/components/keys.js
@@ -5,11 +5,11 @@
 /* Get your api key 👇👇👇👇 */
 /* https://developers.google.com/custom-search/v1/overview#api_key*/
 
-const API_KEY = "Your API KEY";
+const API_KEY = "AIzaSyCucoKh-RbLxQAYLGwofz22k5N-ZVt1Pfw";
 
-/* Get your   Search engine ID and place in CONTEXT_KEY*/ 
+/* Get your   Search engine ID and place in CONTEXT_KEY*/
 /* https://cse.google.co.in/cse/all */
-const CONTEXT_KEY = "CONTEXT_KEY";
+const CONTEXT_KEY = "880a358456389c8b6";
 
 
 const baseURL = "https://www.googleapis.com/customsearch/v1";
